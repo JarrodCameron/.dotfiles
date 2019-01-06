ab282076cc5fdb4ee78409acbce6f8453d4fd084
z5210220 <z5210220@ad.unsw.edu.au>
2019-01-07 10:06:04 +1100

    Still moving files around...

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Dot files for:
 - Vim
 - i3
 - i3blocks
-- bash
+- Bash
+- Handy scripts
--- autogen.sh
+++ autogen.sh
@@ -2,14 +2,45 @@
 
 # Author: Jarrod Cameron (z5210220)
 
-# Link the vim stuff
-ln -s "$HOME"/.dotfiles/vim "$HOME"/.vim
+# TODO:
+# - Create links depending on args
+#   - `--full' will do everything
+#   - `--help' will display usage
+#   - `$name' will link the $name
 
-ln -s "$HOME"/.dotfiles/i3 "$HOME"/.config/i3
-ln -s "$HOME"/.dotfiles/i3blocks "$HOME"/.config/i3blocks
+usage () {
+  echo "TODO: usage"
+}
 
-ln -s "$HOME"/.dotfiles/bashrc "$HOME"/.bashrc
+# If no args
+if [ -z $1 ]; then
+  usage
+  exit 0
+fi
 
+options=""
+for arg in "$@"; do
+  case "$arg" in
+    "vim")      options+="{vim}"      ;;
+    "i3")       options+="{i3}"       ;;
+    "i3blocks") options+="{i3blocks}" ;;
+    "bash")     options+="{bash}"     ;;
+    "scripts")  options+="{scripts}"  ;;
+    *) usage ; exit 0                 ;;
+  esac
+done
 
-
-
+if [ "$(grep "{vim}" <<< "$options")" ]; then
+  echo "AAA"
+fi
+## Link the vim stuff
+#ln -s "$HOME"/.dotfiles/files/vim "$HOME"/.vim
+#
+#ln -s "$HOME"/.dotfiles/files/i3 "$HOME"/.config/i3
+#ln -s "$HOME"/.dotfiles/files/i3blocks "$HOME"/.config/i3blocks
+#
+## .bashrc
+#ln -s "$HOME"/.dotfiles/files/bashrc "$HOME"/.bashrc
+#
+## Personal scripts
+#ln -s "$HOME"/.dotfiles/files/scripts "$HOME"/.scripts
