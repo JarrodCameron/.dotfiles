341f64f3e8c6d5add581aad74e6d4f633d176532
JarrodCameron <z5210220@ad.unsw.edu.au>
2019-09-07 09:19:11 +1000

    Backing up

--- files/.scripts/dwm/menubar.sh
+++ files/.scripts/dwm/menubar.sh
@@ -6,9 +6,8 @@
 # TODO:
 # - memort
 # - processors
-# - wifi battery
-# - volume
-# - date
+# - wifi
+# - battery @ /sys/class/power_supply/BAT0/*
 
 count=0
 
@@ -16,10 +15,24 @@ get_date () {
     printf "%s" "$(date '+%A, %B, %d-%m-%Y %I:%M %p')"
 }
 
+get_vol () {
+    # NOTE: Only tested with single sink
+    local left="$(pacmd list-sinks | awk -F' ' '/volume: front/ {print $5}' | head -n1)"
+    local right="$(pacmd list-sinks | awk -F' ' '/volume: front/ {print $12}' | head -n1)"
+
+    # Trim last char (the `%')
+    left=${left%?}
+    right=${right%?}
+
+    printf "Vol: %s" "$(echo "($left + $right) / 2" | bc )"
+}
+
 while [ "1" ]
 do
 
     title="$(get_date)"
+
+    title="$(get_vol)"" | $title"
 
     xsetroot -name "$title"
 
