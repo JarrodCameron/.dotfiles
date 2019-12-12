f26cf351ada9083b527cbb6a60267bf44f4acc5a
z5210220 <z5210220@ad.unsw.edu.au>
2019-12-12 22:05:10 +1100

    restore.sh works for suckless utils

--- restore.sh
+++ restore.sh
@@ -12,7 +12,7 @@ DOTFILES_HOME="${HOME}""/.dotfiles/"
 DOTFILES_SRC="${DOTFILES_HOME}""/files/"
 DOTFILES_SUCK="${DOTFILES_HOME}""/suckless/"
 
-# All the files are int $DOTFILES_SRC
+# All the files are in $DOTFILES_SRC
 # They are 'restored' by being pointed to with symlinks
 # restore <name> <path>
 #  <name> -> The name of the file (e.g. ".bashrc", ".vim")
@@ -45,53 +45,53 @@ restore () {
   fi
 }
 
+# Clone the git repository. If the repository exists then refresh.
+# apply the patch "${name}.diff" to the repository
+#  <name> -> The name of the file (e.g. ".bashrc", ".vim")
+#  <uri>  -> Location of repository to clone
+restore_suckless () {
+  local name="$1"
+  local uri="$2"
+  if [ -r "${DOTFILES_SRC}/${name}.diff" ]; then
+    if [ -d "${DOTFILES_SUCK}/${name}" ]; then
+      git -C "${DOTFILES_SUCK}/${name}" reset --hard 2>/dev/null
+    else
+      git clone "${uri}" "${DOTFILES_SUCK}/${name}"
+    fi
+    echo "${GREEN}${name} successfully restored${RESET}"
+    git -C "${DOTFILES_SUCK}/${name}" apply "${DOTFILES_SRC}/${name}.diff"
+    local retval="$?"
+    if [ "${retval}" = "0" ]; then
+      echo "${GREEN}${name} successfully patched${RESET}"
+    else
+      echo "${RED}${name} patch failed${RESET}"
+    fi
+  fi
+}
+
 # Create a `files' folder to store the files
 if [ ! -d "${DOTFILES_SRC}" ]; then
   mkdir "${DOTFILES_SRC}"
   echo "${BOLD}""Creating directory in "'$DOTFILES_SRC'"${RESET}"
 fi
 
-# When adding/removing file remember to modify `backup.sh'
-restore "i3"          "${HOME}""/.config/i3"
-restore ".bashrc"     "${HOME}""/.bashrc"
-restore ".scripts"    "${HOME}""/.scripts"
-restore "i3blocks"    "${HOME}""/.config/i3blocks"
-restore ".Xresources" "${HOME}""/.Xresources"
-restore ".tmux.conf"  "${HOME}""/.tmux.conf"
-restore ".vim"        "${HOME}""/.vim"
-restore "radare2rc"   "${HOME}""/.config/radare2/radare2rc"
-
+# Create a `suckless' folder
 if [ ! -d "${DOTFILES_SUCK}" ]; then
   mkdir "${DOTFILES_SUCK}"
   echo "${BOLD}""Creating directory in "'$DOTFILES_SUCK'"${RESET}"
 fi
 
-if [ -r "${DOTFILES_SRC}/dwm.diff" ]; then
-    if [ -d "${DOTFILES_SUCK}/dwm" ]; then
-        git -C "${DOTFILES_SUCK}/dwm" reset --hard 2>/dev/null
-    else
-        git clone git://git.suckless.org/dwm "${DOTFILES_SUCK}/dwm"
-    fi
-    echo "${GREEN}""dwm restored and patched""${RESET}"
-    git -C "${DOTFILES_SUCK}/dwm" apply "${DOTFILES_SRC}/dwm.diff"
-fi
+# When adding/removing file remember to modify `backup.sh'
+#restore "i3"          "${HOME}""/.config/i3"
+#restore ".bashrc"     "${HOME}""/.bashrc"
+#restore ".scripts"    "${HOME}""/.scripts"
+#restore "i3blocks"    "${HOME}""/.config/i3blocks"
+#restore ".Xresources" "${HOME}""/.Xresources"
+#restore ".tmux.conf"  "${HOME}""/.tmux.conf"
+#restore ".vim"        "${HOME}""/.vim"
+#restore "radare2rc"   "${HOME}""/.config/radare2/radare2rc"
 
-if [ -r "${DOTFILES_SRC}/dmenu.diff" ]; then
-    if [ -d "${DOTFILES_SUCK}/dmenu" ]; then
-        git -C "${DOTFILES_SUCK}/dmenu" reset --hard 2>/dev/null
-    else
-        git clone https://git.suckless.org/dmenu "${DOTFILES_SUCK}/dmenu"
-    fi
-    echo "${GREEN}""dmenu restored and patched""${RESET}"
-    git -C "${DOTFILES_SUCK}/dmenu" apply "${DOTFILES_SRC}/dmenu.diff"
-fi
-
-if [ -r "${DOTFILES_SRC}/st.diff" ]; then
-    if [ -d "${DOTFILES_SUCK}/st" ]; then
-        git -C "${DOTFILES_SUCK}/st" reset --hard 2>/dev/null
-    else
-        git clone https://git.suckless.org/st "${DOTFILES_SUCK}/st"
-    fi
-    echo "${GREEN}""st restored and patched""${RESET}"
-    git -C "${DOTFILES_SUCK}/st" apply "${DOTFILES_SRC}/st.diff"
-fi
+# Suckless utils don't care about file system path
+restore_suckless "dwm"   'git://git.suckless.org/dwm'
+restore_suckless "dmenu" 'https://git.suckless.org/dmenu'
+restore_suckless "st"    'https://git.suckless.org/st'
