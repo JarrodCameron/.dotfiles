47798f4ecc79ae10c0c536661f54a779456f336d
z5210220 <z5210220@ad.unsw.edu.au>
2020-12-14 17:04:51 +1100

    Adding final wallpapers

---
+++ files/wallpapers/wallpapers/dl.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Author: Jarrod Cameron (z5210220)
+# Date:   30/08/20 19:02
+
+# ffmpeg -i <input file> <output file>
+
+# Exit on non-zero return status
+set -e
+
+wget https://w.wallhaven.cc/full/ym/wallhaven-ymz8e7.jpg
+wget https://w.wallhaven.cc/full/j5/wallhaven-j563wm.jpg
+wget https://w.wallhaven.cc/full/e7/wallhaven-e7ozyw.jpg
+wget https://w.wallhaven.cc/full/l3/wallhaven-l3968y.jpg
+wget https://w.wallhaven.cc/full/lm/wallhaven-lme3zl.jpg
+wget https://w.wallhaven.cc/full/g8/wallhaven-g83g2l.jpg
+wget https://w.wallhaven.cc/full/q2/wallhaven-q29lml.png
+wget https://w.wallhaven.cc/full/v9/wallhaven-v9mp1p.jpg
+wget https://w.wallhaven.cc/full/wq/wallhaven-wqo9yp.jpg
+wget https://w.wallhaven.cc/full/96/wallhaven-9639ek.jpg
+wget https://w.wallhaven.cc/full/j5/wallhaven-j56mry.jpg
+wget https://w.wallhaven.cc/full/md/wallhaven-md8rw8.jpg
+wget https://w.wallhaven.cc/full/13/wallhaven-137olg.jpg
+wget https://w.wallhaven.cc/full/o3/wallhaven-o39jj9.jpg
+wget https://w.wallhaven.cc/full/6k/wallhaven-6k972x.png
+wget https://w.wallhaven.cc/full/rd/wallhaven-rd7ro1.jpg
