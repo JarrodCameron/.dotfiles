f8738ba2e9c1345862741135f4ef226d2b61490b
Jarrod Cameron <41527221+JarrodCameron@users.noreply.github.com>
2022-04-02 16:18:59 +1100

    Update open_fuzzy.sh

--- files/scripts/.scripts/.bash/open_fuzzy.sh
+++ files/scripts/.scripts/.bash/open_fuzzy.sh
@@ -3,8 +3,7 @@
 # Author: Jarrod Cameron (z5210220)
 # Date:   07/10/19 18:34
 
-IFS=''
-source_file="$(find -type f 2>/dev/null | grep -v '\.class$' | fzf --layout=reverse --margin=3 --header="pwd: $(pwd)" --inline-info --cycle)"
+source_file="$(find ! -name '*.class' -type f 2>/dev/null | fzf --layout=reverse --margin=3 --header="pwd: $(pwd)" --inline-info --cycle)"
 
 if [ -z "$source_file" ]
 then
